21e3d1c1e08e5129b5441e4d867bd1dc3e3ab238
Shaurye Bhatnagar <bhatnagar.shaurye@gmail.com>
2017-09-14 18:04:06 -0500

    Second Iteration
    Organized

---
+++ Design patterns/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromone = Singleton.Getinstance;
+            fromone.print_details("First instance");
+
+           Singleton fromtwo = Singleton.Getinstance;
+            fromtwo.print_details("Second Instance");
+        }
+    }
+}
