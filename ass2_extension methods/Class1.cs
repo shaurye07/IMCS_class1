20f1f882fa11dbc534c82bee1517d9c04f1b138f
Shaurye Bhatnagar <bhatnagar.shaurye@gmail.com>
2017-09-14 23:57:47 -0500

    Organization
    Changes

---
+++ ass2_extension methods/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public static class NameShorter
+    {
+        public static string Shortmyname(this String n1,  String n2, String n3)
+        {
+            StringBuilder s1 = new StringBuilder();
+
+            if (n1.Length>0)
+            {
+                char[] chararr1 = n1.ToCharArray();
+                if (chararr1.Length > 5)
+                {
+                    chararr1[0] = char.ToUpper(chararr1[0]);
+                    s1.Append(chararr1[0] + ".");
+                }
+
+                char[] chararr2 = n2.ToCharArray();
+                if (chararr2.Length > 2)
+                {
+                    chararr2[0] = char.ToUpper(chararr2[0]);
+                    s1.Append(chararr2[0] + ".");
+                }
+
+                s1.Append(n3);
+            }
+
+            return s1.ToString();
+        }
+    }
+}
---
+++ ass2_extension methods/Extension method.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the First name: ");
+            String F_name = Console.ReadLine();
+            Console.WriteLine("Enter the Middle name: ");
+            String M_name = Console.ReadLine();
+            Console.WriteLine("Enter the Last name: ");
+            String L_name = Console.ReadLine();
+
+            String formal_name = NameShorter.Shortmyname(F_name, M_name, L_name);
+
+            Console.WriteLine("Your formal name is : "+formal_name);
+            Console.ReadKey();
+        }
+    }
+}
