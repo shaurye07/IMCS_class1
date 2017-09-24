50ed21b9e1be0748b4f058f547ce3f3b710819b9
Shaurye Bhatnagar <bhatnagar.shaurye@gmail.com>
2017-09-23 23:45:06 -0500

    ass5nksjdnka
    kdsbfks

---
+++ MvcApplication1/MvcApplication1/Common/UserAgentsInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web;
+using System.IO;
+
+
+namespace MvcApplication1.Common
+{ 
+    
+    public class UserAgentsInfo: ActionFilterAttribute     
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            DateTime now = filterContext.RequestContext.HttpContext.Timestamp;
+            String date = now.ToString();
+            String agent = filterContext.HttpContext.Request.UserAgent;
+            base.OnActionExecuting(filterContext);
+            string message = ("\n User logged in using at"+date+" by using"+agent+".");
+            Logdata(message);
+            
+        }
+        private void Logdata(string msg)
+        {
+            
+            
+            using (StreamWriter sw = File.AppendText("~/Data/Data.txt"))
+            {
+                sw.WriteLine(msg);
+                sw.WriteLine("------------------------------------------------");
+            }
+        }
+    }
+}
---
+++ MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Common;
+
+namespace MvcApplication1.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        [UserAgentsInfo]
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
